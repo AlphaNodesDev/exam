cced641b389294c6da805d1760754af7c0ed8bf3
AlphaNodesDev <shyjuss8129@gmail.com>
2024-03-08 12:13:41 +0530

    first commit

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+$dbcon=mysqli_connect("localhost","root","","exam");
+//mysql_select_db("student_placement");
+$hd="YOUNUS COLLEGE OF POLYTECHNIC";
+$footr="<span class='blue bolder'>Project</span> &copy; Trinity Technologies";
+?>
